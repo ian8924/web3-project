378fdc3eb169235cdf2d93d3d5fc0d02833488a8
ian <justforian0915@gmail.com>
2022-10-08 13:22:44 +0800

    update react version 17 -> 18

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import "@rainbow-me/rainbowkit/styles.css";
 import {
   ConnectButton,
   getDefaultWallets,
-  RainbowKitProvider
+  RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import { AccountBoard } from "../components/AccountBoard";
 import { Contract } from "../components/Contract";
 import React from "react";
@@ -20,37 +22,39 @@ const { chains, provider } = configureChains(
 
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
-  chains
+  chains,
 });
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
+  provider,
 });
 
 export default function IndexPage() {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <div
-          style={{
-            width: "100vw",
-            height: "100vh",
-            padding: "50px"
-            // display: "flex",
-            // alignItems: "center",
-            // justifyContent: "center"
-          }}
-        >
-          <ConnectButton />
-          {/* nonSSR -> prevent hydration error */}
-          <NonSSRWrapper>
-            <AccountBoard></AccountBoard>
-            <Contract />
-          </NonSSRWrapper>
-          {/* <h2> {address}</h2> */}
-        </div>
+        <ChakraProvider>
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              padding: "50px",
+              // display: "flex",
+              // alignItems: "center",
+              // justifyContent: "center"
+            }}
+          >
+            <ConnectButton />
+            {/* nonSSR -> prevent hydration error */}
+            <NonSSRWrapper>
+              <AccountBoard></AccountBoard>
+              <Contract />
+            </NonSSRWrapper>
+            {/* <h2> {address}</h2> */}
+          </div>
+        </ChakraProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+
+export default function ProfilePage() {
+    return (
+        <ChakraProvider>
+            <div>Profile Page</div>
+        </ChakraProvider>
+    )
+  }
+  
