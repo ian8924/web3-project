db274dcf916343649ed5569328691ca10c5e34df
Somer <somer.yang@tkspring.com>
2022-11-03 20:37:05 +0800

    add ChakraProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,27 @@
-
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
-import '../styles/globals.scss';
-import '../styles/homePage.scss';
+import "../styles/globals.scss";
+import "../styles/homePage.scss";
+
+const colors = {};
+const theme = extendTheme({
+  // styles: {
+  //   global: () => ({
+  //     body: {
+  //       bg: "",
+  //       color: "",
+  //       fontFamily: "",
+  //     },
+  //   }),
+  // },
+  colors,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 
