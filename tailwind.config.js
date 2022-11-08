cb010c17f8cf8aaea5036a5c4628e5728babf7d4
Somer <somer.yang@tkspring.com>
2022-11-08 13:30:05 +0800

    icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
+  corePlugins: {
+    preflight: false,
+  },
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
