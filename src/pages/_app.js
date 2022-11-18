14922c90c7b958afeb8ca79167a43628af2d8e13
ian <justforian0915@gmail.com>
2022-11-18 21:01:44 +0800

    fix: css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -46,7 +46,7 @@ import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [chain.mainnet, chain.goerli],
   [
     alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
