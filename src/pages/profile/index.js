f5e12267f1bb495e1826e2aad1daa1c7c6adadb4
sa556693828 <sa556693828@gmail.com>
2022-12-01 21:07:39 +0800

    TokenURI

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -4,24 +4,28 @@ import Background from "../../components/Background/Background";
 import ContentBg from "../../components/Background/ContentBg";
 import Alert from "../../components/Alert/Alert";
 import { getContract } from "../../hooks/useContract";
+import { decode } from "js-base64";
 
 export default function ProfilePage() {
   const [contractToken, setContractToken] = useState();
+  const [tokenURI, setTokenURI] = useState();
   const getData = useCallback(async () => {
-    const contract = await getContract()
+    const contract = await getContract();
     setContractToken(contract);
   }, []);
-  
   const getTokenURI = useCallback(async () => {
-    const tokenURI = await contractToken?.tokenURI(1);
-    console.log(tokenURI);
+    const tokenURI = await contractToken?.tokenURI(2);
+    const object = JSON.parse(
+      decode(tokenURI.replace("data:application/json;base64,", ""))
+    );
+    setTokenURI(object.image);
   }, [contractToken]);
 
   useEffect(() => {
     getData();
   }, []);
   useEffect(() => {
-    if(contractToken){
+    if (contractToken) {
       getTokenURI();
     }
   }, [contractToken]);
@@ -42,11 +46,7 @@ export default function ProfilePage() {
         {console.log("s", contractToken)}
         {/* {console.log("metadata",metadata)} */}
         <Center h={"calc(100vh - 80px)"} w={"100vw"}>
-          <iframe
-            className="h-full w-3/5 py-6"
-            src="https://www.w3schools.com"
-            title="Profile NFT"
-          />
+          <img width={"100vw"} height={"100vh"} src={tokenURI} />
         </Center>
       </Box>
     </ContentBg>
