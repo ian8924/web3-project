a9e7643fec42c36f26c3ec8167eaa51e79797244
sa556693828 <sa556693828@gmail.com>
2022-12-08 01:53:25 +0800

    get NFT animation

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,11 +1,15 @@
-import { ethers } from 'ethers';
-import ERC721_ABI from '../components/Abi/ERC721-ABI.json';
-const goerliAPI="https://goerli.infura.io/v3/daf792c59ab34774b94d37ed208b41fe"
-const ethAPI ="https://mainnet.infura.io/v3/daf792c59ab34774b94d37ed208b41fe"
+import { ethers } from "ethers";
+import ERC721_ABI from "../components/Abi/ERC721-ABI.json";
+const goerliAPI =
+  "https://goerli.infura.io/v3/daf792c59ab34774b94d37ed208b41fe";
+const ethAPI = "https://mainnet.infura.io/v3/daf792c59ab34774b94d37ed208b41fe";
+const contract = "0x921eb8e0305b428a9eed6cc228f92ca3073bd4e0";
 
-export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(goerliAPI);
+export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(
+  goerliAPI
+);
 
-export const getContract = (abi = ERC721_ABI, address='0x80DA9A173F4AE036a8cc060B602153FC7b48b763', signer) => {
+export const getContract = (abi = ERC721_ABI, address = contract, signer) => {
   const signerOrProvider = signer ?? simpleRpcProvider;
   return new ethers.Contract(address, abi, signerOrProvider);
 };
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -8,18 +8,28 @@ import { decode } from "js-base64";
 
 export default function ProfilePage() {
   const [contractToken, setContractToken] = useState();
-  const [tokenURI, setTokenURI] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [animationUrl, setAnimationUrl] = useState();
+  const [bgColor, setBgColor] = useState();
+  const tilte="Here is your NFT , come and play your NFT right now"
+
+  // tokenOfOwnerByIndex tokenID
+  // balanceOf 數量
+  
   const getData = useCallback(async () => {
     const contract = await getContract();
     setContractToken(contract);
   }, []);
   const getTokenURI = useCallback(async () => {
-    const tokenURI = await contractToken?.tokenURI(2);
+    const tokenURI = await contractToken?.tokenURI(12);
     const object = JSON.parse(
       decode(tokenURI.replace("data:application/json;base64,", ""))
     );
-    setTokenURI(object.image);
-    // console.log("tokenURI",tokenURI)
+    setImageUrl(object.image);
+    const url=object.animation_url.replace("ipfs://", "")
+    setAnimationUrl(url);
+    setBgColor("bg-[#"+object.attributes[0].value+"]")
+    console.log("object", object);
   }, [contractToken]);
 
   useEffect(() => {
@@ -32,20 +42,26 @@ export default function ProfilePage() {
   }, [contractToken]);
   return (
     <ContentBg>
-      <Box className="" zIndex="5">
+      <Box  zIndex="5">
         <Flex
           w={"100%"}
           pt={6}
-          className="text-2xl"
-          color={"black"}
+          className="strokeText text-2xl"
+          data-storke={tilte}
+          color={"yellow"}
           justify={"center"}
         >
-          遊戲說明
-          {/* <Alert>ss</Alert> */}
+          {tilte}
         </Flex>
         {/* //TODO: Skeleton before get NFT */}
         <Center h={"calc(100vh - 80px)"} w={"100vw"}>
-          <img width={"100vw"} height={"100vh"} src={tokenURI} />
+          {/* <img width={"100vw"} height={"100vh"} src={imageUrl} /> */}
+          <iframe
+          className={`${bgColor} w-5/6 h-[50%] md:h-1/2 md:w-5/6 lg:h-2/3 xl:h-3/4 xl:w-3/4 2xl:w-8/12`}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          sandbox="allow-scripts"
+          src={"http://ipfs.io/ipfs/"+animationUrl}
+          />
         </Center>
       </Box>
     </ContentBg>
