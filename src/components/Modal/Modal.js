f80e705c083193512aa55450bb69e4288483e03e
ian <justforian0915@gmail.com>
2022-12-11 07:51:56 +0800

    feat : Question 彈窗

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,104 @@
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import NoSSRWrapper from "../NoSSRWrapper";
+
+import {
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  useDisclosure,
+  RadioGroup,
+  Stack,
+  Radio,
+} from "@chakra-ui/react";
+
+const ModalQuestion = forwardRef((props, ref) => {
+  // 彈窗控制
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [value, setValue] = useState("");
+
+  useImperativeHandle(ref, () => ({
+    // 開啟彈窗
+    openModal() {
+      onOpen();
+    },
+    // 關閉彈窗
+    closeModal() {
+      onClose();
+    },
+  }));
+
+  return (
+    <>
+      <NoSSRWrapper>
+        <Button onClick={onOpen}>Open Drawer</Button>
+        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent marginRight={"12px"} marginLeft={"12px"}>
+            <ModalHeader color={"rgb(0,139,139)"} fontSize={"16px"}>
+              Please answer the question to continue ...
+            </ModalHeader>
+            <ModalBody>
+              <Box color={"black"} fontSize={"20px"} marginBottom={"20px"}>
+                What advantages does ZK-Rollup offer over traditional blockchain
+                technology?
+              </Box>
+
+              <RadioGroup onChange={setValue} value={value} marginTop={"12px"}>
+                <Stack direction={"column"} color={"black"} gap={"12px"}>
+                  <Radio
+                    value="1"
+                    spacing={"10px"}
+                    size={"lg"}
+                    colorScheme={"green"}
+                  >
+                    1. Let users to get higher APY in DeFi
+                  </Radio>
+                  <Radio
+                    value="2"
+                    spacing={"10px"}
+                    size={"lg"}
+                    colorScheme={"green"}
+                  >
+                    2. Allow users to store crypto more secure than cold wallet
+                  </Radio>
+                  <Radio
+                    value="3"
+                    spacing={"10px"}
+                    size={"lg"}
+                    colorScheme={"green"}
+                  >
+                    3. Increased scalability, improved privacy, and reduced
+                    storage and bandwidth requirements
+                  </Radio>
+                </Stack>
+              </RadioGroup>
+            </ModalBody>
+
+            <ModalFooter marginTop={"16px"}>
+              <Button bgColor={"#ccc"} mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button
+                bgColor={"rgb(0,139,139)"}
+                isDisabled={value ? false : true}
+                onClick={() => {
+                  props.confirmQuestion(value);
+                }}
+              >
+                Submit
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </NoSSRWrapper>
+    </>
+  );
+});
+
+export default ModalQuestion;
--- src/pages/mint/index.js
+++ src/pages/mint/index.js
@@ -1,13 +1,15 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { Box } from "@chakra-ui/react";
 import ContentBg from "../../components/Background/ContentBg";
 import Wave3 from "../../components/Background/Wave3";
-import pop from "../../assets/images/pop.png";
 import { useSignMessage, useAccount } from "wagmi";
 import axios from "axios";
 import moment from "moment";
+import ModalQuestion from "../../components/Modal/Modal";
 
 export default function ProfilePage() {
+  const modalRef = useRef();
+  // 帳戶
   const { isConnected, address } = useAccount();
   // 簽章資料
   const [signData, setSignData] = useState();
@@ -17,20 +19,21 @@ export default function ProfilePage() {
   const [amount, setAmount] = useState(0);
   // 所有 mint 地址
   const [allInfo, setAllInfo] = useState([]);
+  // 問題
+  const [question, setQuestion] = useState("");
+
   const {
     data,
     signMessage,
     signMessageAsync: signAuth,
   } = useSignMessage({
-    message: "Mint Arjaverse NFT",
+    message: `Mint Arjaverse NFT , Question=1`,
   });
 
   const getAmout = () => {
     axios.get("https://api.arjaverse.art/nft/mint-info").then((res) => {
-      // setAmount(0);
       setAmount(res.data.availableAmt);
       setDeadline(res.data.deadline);
-      // setDeadline(0);
     });
   };
   const getAllmint = () => {
@@ -51,7 +54,7 @@ export default function ProfilePage() {
     return idx !== -1;
   };
 
-  //call useSign 確認拿到簽章
+  // call useSign 確認拿到簽章
   const authUser = useCallback(async () => {
     if (isConnected) {
       try {
@@ -64,7 +67,7 @@ export default function ProfilePage() {
             //API要求的資料
             data: {
               address,
-              message: "Mint Arjaverse NFT",
+              message: `Mint Arjaverse NFT , Question=${question}`,
               signature: sig,
             },
           })
@@ -88,67 +91,118 @@ export default function ProfilePage() {
     }
   }, [isConnected, signAuth]);
 
+  // 確認問題
+  const confirmQuestion = (val) => {
+    modalRef.current.closeModal();
+    setQuestion(val);
+    authUser();
+  };
+
   return (
-    <ContentBg position="relative">
-      <Box
-        className="where"
-        position="flex"
-        mt={{ base: "5", sm: "10" }}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        zIndex="100"
-      >
+    <>
+      <ModalQuestion ref={modalRef} confirmQuestion={confirmQuestion} />
+      <ContentBg position="relative">
         <Box
-          width={{ base: "200px", sm: "300px" }}
-          height={{ base: "200px", sm: "300px" }}
+          className="where"
+          position="flex"
+          mt={{ base: "5", sm: "10" }}
           display="flex"
+          flexDirection="column"
           justifyContent="center"
           alignItems="center"
-          borderRadius="50%"
-          marginBottom="30px"
-          position="relative"
-          overflow="hidden"
-          border={{ base: "9px solid #425673", sm: "15px solid #425673" }}
+          zIndex="100"
         >
           <Box
-            color="#425673"
-            fontSize={{ base: "30px", sm: "40px" }}
-            zIndex="2"
-            fontWeight="700"
-            textAlign="center"
-            marginTop="-30px"
+            width={{ base: "200px", sm: "300px" }}
+            height={{ base: "200px", sm: "300px" }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            borderRadius="50%"
+            marginBottom="30px"
+            position="relative"
+            overflow="hidden"
+            border={{ base: "9px solid #425673", sm: "15px solid #425673" }}
           >
-            剩餘
-            <Box display="flex" alignItems="flex-end" marginTop="-30px">
-              <Box
-                className="strokeText"
-                data-storke={amount}
-                id="title"
-                fontSize={{ base: "90px !important", sm: "120px !important" }}
-                lineHeight={{ base: "100px", sm: "120px" }}
-              >
-                {amount}
-              </Box>
-              <Box color="#425673" fontSize="24px" zIndex="2" fontWeight="700">
-                個
+            <Box
+              color="#425673"
+              fontSize={{ base: "30px", sm: "40px" }}
+              zIndex="2"
+              fontWeight="700"
+              textAlign="center"
+              marginTop="-30px"
+            >
+              剩餘
+              <Box display="flex" alignItems="flex-end" marginTop="-30px">
+                <Box
+                  className="strokeText"
+                  data-storke={amount}
+                  id="title"
+                  fontSize={{ base: "90px !important", sm: "120px !important" }}
+                  lineHeight={{ base: "100px", sm: "120px" }}
+                >
+                  {amount}
+                </Box>
+                <Box
+                  color="#425673"
+                  fontSize="24px"
+                  zIndex="2"
+                  fontWeight="700"
+                >
+                  個
+                </Box>
               </Box>
             </Box>
+            <Box
+              position="absolute"
+              left="-20px"
+              top={`calc(70% + ${-30}px)`}
+              zIndex="0"
+            >
+              <Wave3></Wave3>
+            </Box>
           </Box>
-          <Box
-            position="absolute"
-            left="-20px"
-            top={`calc(70% + ${-30}px)`}
-            zIndex="0"
-          >
-            <Wave3></Wave3>
-          </Box>
-        </Box>
-        {/* 此帳號是否已經mint */}
-        {getCurrentHasMint() ? (
-          // 是否有過deadline
-          moment().valueOf() > parseInt(deadline) ? (
+          {/* 此帳號是否已經mint */}
+          {getCurrentHasMint() ? (
+            // 是否有過deadline
+            moment().valueOf() > parseInt(deadline) ? (
+              <Box
+                fontSize={{ base: "30px", sm: "50px" }}
+                fontWeight="700"
+                marginBottom="20px"
+                color="red"
+              >
+                所有NFT已發放完畢
+              </Box>
+            ) : (
+              <Box
+                fontSize={{ base: "30px", sm: "50px" }}
+                fontWeight="700"
+                color="#425673"
+                textAlign="center"
+                marginBottom="20px"
+              >
+                此帳號已 mint 過，等待發放中... <br />
+                發放時間： {moment(deadline).format("YYYY/MM/DD , h:mm")}
+              </Box>
+            )
+          ) : amount > 0 ? (
+            <Box
+              onClick={() => modalRef.current.openModal()}
+              as="button"
+              borderRadius="45px"
+              border={{ base: "5px solid #425673", sm: "10px solid #425673" }}
+              width={{ base: "150px", sm: "360px" }}
+              height={{ base: "50px", sm: "90px" }}
+              fontSize={{ base: "20px", sm: "30px" }}
+              fontWeight="700"
+              marginBottom="30px"
+              color="#425673"
+              bg="#fff"
+            >
+              MINT
+            </Box>
+          ) : (
             <Box
               fontSize={{ base: "30px", sm: "50px" }}
               fontWeight="700"
@@ -157,74 +211,39 @@ export default function ProfilePage() {
             >
               所有NFT已發放完畢
             </Box>
-          ) : (
-            <Box
-              ontSize={{ base: "30px", sm: "50px" }}
-              fontWeight="700"
-              color="#425673"
-              textAlign="center"
-              marginBottom="20px"
-            >
-              此帳號已 mint 過，等待發放中... <br />
-              發放時間： {moment(deadline).format("YYYY/MM/DD , h:mm")}
-            </Box>
-          )
-        ) : amount > 0 ? (
+          )}
           <Box
-            onClick={() => authUser()}
-            as="button"
-            borderRadius="45px"
-            border={{ base: "5px solid #425673", sm: "10px solid #425673" }}
-            width={{ base: "150px", sm: "360px" }}
-            height={{ base: "50px", sm: "90px" }}
-            fontSize={{ base: "20px", sm: "30px" }}
-            fontWeight="700"
-            marginBottom="30px"
+            borderRadius="lg"
             color="#425673"
-            bg="#fff"
+            width={{ base: "90%", sm: "80%" }}
+            maxWidth="1100px"
+            backgroundColor="rgba(255,255,255,.5)"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            padding={{ base: "16px", sm: "50px 70px" }}
           >
-            MINT
-          </Box>
-        ) : (
-          <Box
-            fontSize={{ base: "30px", sm: "50px" }}
-            fontWeight="700"
-            marginBottom="20px"
-            color="red"
-          >
-            所有NFT已發放完畢
-          </Box>
-        )}
-        <Box
-          borderRadius="lg"
-          color="#425673"
-          width={{ base: "90%", sm: "80%" }}
-          maxWidth="1100px"
-          backgroundColor="rgba(255,255,255,.5)"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          padding={{ base: "16px", sm: "50px 70px" }}
-        >
-          <Box
-            fontSize={{ base: "24px", sm: "60px" }}
-            marginBottom={{ base: "16px", sm: "24px" }}
-          >
-            Zero-Knowledge rollup
-          </Box>
-          <Box
-            fontSize={{ base: "16px", sm: "24px" }}
-            lineHeight={{ base: "24px", sm: "45px" }}
-          >
-            1. 100 ＝＞ 1 ， 100筆資料打包成1筆上鏈，實現100個人
-            free-mint，只要付一次 Gas <br />
-            2. 大量的 MetaData 上鏈，包裝起來一次上鏈，降低所需的開發成本 <br />
-            3. 項目方的空投活動，不再需要一個一個傳送 <br />
-            4. 令使用者只需利用錢包的簽章，即可 mint NFT，實現 0 Gas fee
-            <br />
+            <Box
+              fontSize={{ base: "24px", sm: "60px" }}
+              marginBottom={{ base: "16px", sm: "24px" }}
+            >
+              Zero-Knowledge rollup
+            </Box>
+            <Box
+              fontSize={{ base: "16px", sm: "24px" }}
+              lineHeight={{ base: "24px", sm: "45px" }}
+            >
+              1. 100 ＝＞ 1 ， 100筆資料打包成1筆上鏈，實現100個人
+              free-mint，只要付一次 Gas <br />
+              2. 大量的 MetaData 上鏈，包裝起來一次上鏈，降低所需的開發成本{" "}
+              <br />
+              3. 項目方的空投活動，不再需要一個一個傳送 <br />
+              4. 令使用者只需利用錢包的簽章，即可 mint NFT，實現 0 Gas fee
+              <br />
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </ContentBg>
+      </ContentBg>
+    </>
   );
 }
