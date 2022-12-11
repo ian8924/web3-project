53bfa197f2784f3da9e6456ff5f8149e0b6dbfe6
ian <justforian0915@gmail.com>
2022-12-11 14:21:57 +0800

    update: signature

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -45,8 +45,7 @@ const ModalQuestion = forwardRef((props, ref) => {
             </ModalHeader>
             <ModalBody>
               <Box color={"black"} fontSize={"20px"} marginBottom={"20px"}>
-                What advantages does ZK-Rollup offer over traditional blockchain
-                technology?
+                Which is the advantage of ZK-Rollup ?
               </Box>
 
               <RadioGroup onChange={setValue} value={value} marginTop={"12px"}>
