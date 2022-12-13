61264b74c56349bfb2b13dd169897b326bbbf46a
sa556693828 <sa556693828@gmail.com>
2022-12-13 21:05:54 +0800

    new Contract

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -3,7 +3,7 @@ import ERC721_ABI from "../components/Abi/ERC721-ABI.json";
 const goerliAPI =
   "https://goerli.infura.io/v3/daf792c59ab34774b94d37ed208b41fe";
 const ethAPI = "https://mainnet.infura.io/v3/daf792c59ab34774b94d37ed208b41fe";
-const contract = "0x03eea51d8a3414df00c32a90bdc2c7bb9aa51f68";
+const contract = "0x7303b1E999e4EbFF83bb515f2E37ab7f5dB1eF3C";
 
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(
   goerliAPI
